852cc11d723ef6f93d0ce3bccb5416bc9bed86b0
Mauro Small <39747093+mlsmall@users.noreply.github.com>
2019-06-27 17:33:41 -0400

    Update my split function.py
    Added comments to my split function

--- my split function.py
+++ my split function.py
@@ -6,28 +6,32 @@ def mysplit(strng):
     if strng == '' or strng.isspace():
         return []
 
-    # prepare a list to return
+    # initialize a list to return
     wordlist = []
-    # prepare a word to build subsequent words
+    # initialize a word to build subsequent words
     word = ""
-    # prepare the last word of the list
+    # initialize the last word of the list
     lastword = ""
 
- 
-    for character in strng:
+    # Making a word from the sum of characters before an empty space
+    # And then appending that word to the list
+    for character in strng: # For every character inside the string
         word += character
         if character == " ":
             word = word[:-1]
             wordlist.append(word)
             word = ""
             
+    # Index of where the last space is in the string
     index = strng.rfind(' ')
     
+    # Adding the charcters from the last space until the end of the string
     for lastchars in strng[index:]:
         lastword += lastchars
-    
+    # Appending the last character to the list
     wordlist.append(lastword)
     
+    # Removing any additional spaces inside the list
     for word in wordlist:
         if word.isspace() or word == "":
             wordlist.remove(word)
@@ -36,7 +40,7 @@ def mysplit(strng):
     return wordlist
         
         
-
+# Testing the function
 print(mysplit("To be or not to be, that is the question"))
 print(mysplit("To be or not to be,that is the question"))
 print(mysplit("   "))
